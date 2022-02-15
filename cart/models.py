73cac05d2de76b23d8e8b4a7b74efe6585d35554
unknown <michael.ngigi@strathmore.edu>
2022-02-15 18:56:55 +0300

    Defined database model for cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from djongo import models
+from products.models import Product
+
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=255, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.product
