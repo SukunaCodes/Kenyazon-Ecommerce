10c64c2ebfccf8d62b920e8ef05039a12b09f64c
unknown <michael.ngigi@strathmore.edu>
2022-02-15 15:20:19 +0300

    Added single_product functionality

--- store/views.py
+++ store/views.py
@@ -8,7 +8,6 @@ from subcategory.models import Subcategory
 
 
 def store(request, category_slug=None, subcategory_slug=None):
-
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available__in=[True])
@@ -28,3 +27,18 @@ def store(request, category_slug=None, subcategory_slug=None):
         'product_count': product_count,
     }
     return render(request, template_name='store/store.html', context=context)
+
+
+# Product detail functionality
+def product_detail(request, category_slug=None, subcategory_slug=None, product_slug=None):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, subcategory__slug=subcategory_slug,
+                                             slug=product_slug)
+
+    except Exception as e:
+        raise e
+
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, template_name='store/product_detail.html', context=context)
